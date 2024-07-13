de6484cc179ac71b342e3c626c684d992dc4ec2e
Vedantb27 <bopanwarvedant27@gmail.com>
2024-07-13 15:23:38 +0530

    Resume Download functionality Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,29 @@
-import logo from "../assets/kevinRushLogo.png"
-import {FaLinkedin} from "react-icons/fa"
-import {FaGithub} from "react-icons/fa"
+import React from 'react';
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import resume from "../assets/VedantBopanwar(BE-CE)Resume.pdf"; // Ensure this path is correct
 
 const Navbar = () => {
   return (
-    <nav className="
-      flex items-center justify-between py-4">
-        <div className="flex flex-shrink-0 items-center" >
-            <img className="mx-2 w-7" src={logo}  alt="logo"/>
-        </div>
-        <div className="m-5 flex item-center justify-center gap-4 text-2xl " >
-            <FaLinkedin className="hover:text-cyan-700" />
-            <FaGithub  className="hover:text-white"/>
-            <button className="bg-transparent hover:bg-blue-500 text-slate-4000 font-semibold hover:text-white  px-4 border border-slate-400 hover:border-transparent rounded">Resume</button>
-        </div>
+    <nav className="flex items-center justify-between py-4">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-7" src={logo} alt="logo" />
+      </div>
+      <div className="m-5 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin className="hover:text-cyan-700" />
+        <FaGithub className="hover:text-white" />
+        <a
+          href={resume}
+          download="VedantBopanwar(BE-CE)Resume.pdf" // This specifies the name of the file to be downloaded
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-transparent hover:bg-blue-500 text-slate-4000 font-semibold hover:text-white px-4 border border-slate-400 hover:border-transparent rounded"
+        >
+          Resume
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
