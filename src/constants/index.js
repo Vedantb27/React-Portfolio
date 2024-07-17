371f91776cb7f325a168a892f396b89d688d9c60
Vedantb27 <bopanwarvedant27@gmail.com>
2024-07-17 17:21:23 +0530

    come profile chnages

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,37 +5,27 @@ import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React.js , as well as back-end technologies like Node.js, Express.js, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `
+I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React,Node.js and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    role: " Full Stack Developer",
+    company: "HB GADGET TECHNOLOGY ND SOLUTION PVT LTD",
+    description: `Designed and developed user interfaces for web applications using React.js. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["Javascript", "React.js", "Express.js", "Node.js", "Tailwind", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
+    year: "2023 - 2024",
     role: "Frontend Developer",
     company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    description: `
+Frontend Developer - AnkHub Technology Services
+Designed and developed user interfaces for web applications using React.js. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance..`,
+    technologies: ["Javascript", "React.js", "Express.js", "Node.js", "Tailwind", "mongoDB",],
   },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+
 ];
 
 export const PROJECTS = [
@@ -44,9 +34,9 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional Food Blog  website with features like Dishes Recepie listing, Serach Feature, and admin authentication for adding new recipies , deleting old recepies , updating recipie or creating diffrent categories. A fully functional Food Blog  website with features like Dishes Recepie listing, Serach Feature, and admin authentication for adding new recipies , deleting old recepies , updating recipie or creating diffrent categories. A fully functional Food Blog  website with features like Dishes Recepie listing, Serach Feature, and admin authentication for adding new recipies , deleting old recepies , updating recipie or creating diffrent categories.",
-    technologies: ["HTML", "CSS","Tailwind CSS", "React", "Node.js","Express.js", "MongoDB"],
-    githublink:"https://github.com/Vedantb27/Foodblogfrontend",
-    livelink:"https://foodbloglive.vercel.app",
+    technologies: ["HTML", "CSS", "Tailwind CSS", "React", "Node.js", "Express.js", "MongoDB"],
+    githublink: "https://github.com/Vedantb27/Foodblogfrontend",
+    livelink: "https://foodbloglive.vercel.app",
   },
   {
     title: "Local Storage Notes Website",
@@ -54,31 +44,23 @@ export const PROJECTS = [
     description:
       "A dynamic web-based tool for managing notes. Users can create, save, delete, and color-code notes with ease. Notes are stored in localStorage for persistence, and the interface is responsive and interactive, featuring Font Awesome icons. Technologies used include HTML5, CSS3, and JavaScript.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
-    githublink:"https://github.com/Vedantb27/LocalStorageNotes",
-    livelink:"https://vedantb27.github.io/LocalStorageNotes/",
+    githublink: "https://github.com/Vedantb27/LocalStorageNotes",
+    livelink: "https://vedantb27.github.io/LocalStorageNotes/",
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-    githublink:"",
-    livelink:"",
+      "Explore my portfolio crafted with HTML, CSS, and Tailwind CSS for stunning design, powered by React.js for dynamic interactivity. Integrated with EmailJS for seamless contact forms, Framer Motion for smooth animations, and React-Toast for user-friendly notifications.",
+    technologies: [ "React.js", "Tailwind css", "EmailJS","Framer-Motion" ,"React-Toast"],
+    githublink: "https://github.com/Vedantb27/React-Portfolio",
+    livelink: "https://vedantbopanwar-portfolio-three.vercel.app/",
   },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-    githublink:"",
-    livelink:"",
-  },
+  
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Dighori nagpur, maharashtra ,india",
+  phoneNo: "+91 9604501071 ",
+  email: "bopanwarvedant27@gmail.com",
 };
